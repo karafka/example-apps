29845b7c53fe317dafff8e8a177d74a4c0d32bf0
pavlo_vavruk <pavlo.vavruk@gmail.com>
2015-08-25 11:31:15 +0200

    Add Readme

--- app/tasks/sender.rake
+++
@@ -1,6 +0,0 @@
-namespace :waterdrop do
-  desc 'Runs a single Karafka processing instance'
-  task :send do
-    Generator.new.send_messages
-  end
-end
--- config/initializers/water_drop.rb
+++
@@ -1,7 +0,0 @@
-::WaterDrop.setup do |config|
-  config.send_messages = true
-  config.kafka_host = '127.0.0.1'
-  config.kafka_ports = ['9093']
-  config.connection_pool_size = 25
-  config.connection_pool_timeout = 1
-end
