62112ee122cb7cb17f360a1aabd1a9bdbbbeb470
Adam Gwozdowski <agwozdowski@striketech.pl>
2015-10-15 14:39:02 +0200

    Redis config for new karafka

--- app.rb
+++ app.rb
@@ -10,7 +10,8 @@ class App < Karafka::App
     config.worker_timeout = 60 # 1 minute
     config.concurrency = 5
     config.name = 'example_app'
-    config.redis_host = 'redis://localhost:6379'
+    config.redis_url = 'redis://localhost:6379'
+    config.redis_namespace = 'example_app'
   end
 end
 
