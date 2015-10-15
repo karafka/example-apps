9af9d27bef03e2a103f69de47790c636ff303868
Maciej Mensfeld <maciej@mensfeld.pl>
2015-10-15 11:40:44 +0200

    waterdrop config consistency + gem dump

--- config/initializers/water_drop.rb
+++ config/initializers/water_drop.rb
@@ -1,7 +1,6 @@
 ::WaterDrop.setup do |config|
   config.send_messages = true
-  config.kafka_host = '127.0.0.1'
-  config.kafka_ports = ['9092']
+  config.kafka_hosts = ['127.0.0.1:9092']
   config.connection_pool_size = 25
   config.connection_pool_timeout = 1
   config.raise_on_failure = true
