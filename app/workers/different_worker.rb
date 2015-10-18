7c745276800a504484231cda5c071ee0f2886561
Maciej Mensfeld <maciej@mensfeld.pl>
2015-10-18 11:08:10 +0200

    yard typo

--- app/workers/different_worker.rb
+++ app/workers/different_worker.rb
@@ -1,6 +1,6 @@
 # Dummy worker that just proves that we can use custom workers with Karafka controllers
 class DifferentWorker
-  # @param params Any params that we want to have
+  # @param _params Any params that we want to have
   # @return [Boolean] false always
   def perform_async(*_params)
     false
