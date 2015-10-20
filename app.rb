781f67813905a4bbc607d7cd70ae7c1b1d66417e
Adam Gwozdowski <agwozdowski@striketech.pl>
2015-10-20 13:15:53 +0200

    Xml message, change kafka address to localhost

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ Bundler.require(:default, ENV['KARAFKA_ENV'])
 class App < Karafka::App
   setup do |config|
     config.kafka_hosts = %w( 127.0.0.1:9092 )
-    config.zookeeper_hosts = %w( 172.17.1.147:2181 )
+    config.zookeeper_hosts = %w( 127.0.0.1:2181 )
     config.worker_timeout = 60 # 1 minute
     config.concurrency = 5
     config.name = 'example_app'
