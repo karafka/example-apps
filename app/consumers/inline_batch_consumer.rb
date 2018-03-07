529babd2692107bb1c890a1837e532d4cc920739
Maciej Mensfeld <maciej@mensfeld.pl>
2018-03-07 17:30:56 +0100

    rubocop remarks

--- app/consumers/inline_batch_consumer.rb
+++ app/consumers/inline_batch_consumer.rb
@@ -9,8 +9,8 @@ class InlineBatchConsumer < ApplicationConsumer
   def consume
     # You can access the params_batch as an array and just work with it
     sum = params_batch
-            .map { |param| param.fetch('number') }
-            .inject(0){ |sum, val| sum + val }
+          .map { |param| param.fetch('number') }
+          .inject(0) { |accu, val| accu + val }
     Karafka.logger.info "Sum of #{params_batch.count} elements equals to: #{sum}"
   end
 end
