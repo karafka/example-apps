0d340be70cea5e49473743c86ce5ff0323f33445
Maciej Mensfeld <maciej@mensfeld.pl>
2021-11-27 20:27:52 +0100

    manual offset management check

--- v2.0-non-rails/app/consumers/counters_consumer.rb
+++ v2.0-non-rails/app/consumers/counters_consumer.rb
@@ -13,5 +13,9 @@ class CountersConsumer < ApplicationConsumer
           .inject(0) { |accu, val| accu + val }
 
     Karafka.logger.info "Sum of #{messages.count} elements equals to: #{sum}"
+
+    # This is not needed as automatic offset management is on, but we add it just to illustrate
+    # that the karafka-testing works with this case as well
+    mark_as_consumed!(messages.last)
   end
 end
