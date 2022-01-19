423bb613d31d6601a1b3860ebd7872f4e62761b3
Maciej Mensfeld <maciej@mensfeld.pl>
2022-01-19 17:55:51 +0100

    remarks

--- v2.0-rails/karafka.rb
+++ v2.0-rails/karafka.rb
@@ -21,6 +21,7 @@ class KarafkaApp < Karafka::App
   # Karafka.monitor.subscribe(Karafka::Instrumentation::ProctitleListener.new)
 
   routes.draw do
+
     # This needs to match queues defined in your ActiveJobs
     active_job_topic :default
 
