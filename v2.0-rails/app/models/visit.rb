6beac04269abd127243bdb9f7a9c3f0e7f3645f8
Maciej Mensfeld <maciej@mensfeld.pl>
2022-10-19 12:48:50 +0200

    bump and more specs (#187)
    * bump and more specs
    
    * remarks

--- v2.0-rails/app/models/visit.rb
+++ v2.0-rails/app/models/visit.rb
@@ -5,4 +5,18 @@ class Visit < ApplicationRecord
   scope :recent, -> { joins(:visitor).order(visited_at: :desc).limit(20) }
 
   belongs_to :visitor
+
+  # @note This is not used in the example app but is used to demonstrate how we can also spec out
+  # messages production when models or anything else is publishing data
+  def broadcast
+    Karafka.producer.produce_async(
+      topic: 'visits',
+      payload: {
+        id: id,
+        visited_at: visited_at,
+        visitor_id: visitor_id,
+        page_path: page_path
+      }.to_json
+    )
+  end
 end
