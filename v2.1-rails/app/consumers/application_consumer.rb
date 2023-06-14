6b1710f63063e7a4e9b1668d7dcca5bb14da75f7
sarvesh-sankaranarayanan <82436491+sarvesh-sankaranarayanan@users.noreply.github.com>
2023-06-14 11:26:14 +0530

    Initial Commit

--- v2.1-rails/app/consumers/application_consumer.rb
+++ v2.1-rails/app/consumers/application_consumer.rb
@@ -4,4 +4,24 @@
 # You can rename it if it would conflict with your current code base (in case you're integrating
 # Karafka with other frameworks)
 class ApplicationConsumer < Karafka::BaseConsumer
+    def consume
+        @messages = messages
+        messages.each do |message|
+            Rails.logger.tagged(SecureRandom.uuid) do
+                check_visit
+                consume_one
+                mark_as_consumed(message)
+            end
+        end
+    end
+
+    private 
+
+    attr_reader :messages
+
+    # Dummy method to run some SQL queries
+    def check_visit
+        Rails.logger.info "Test print log with UUID tagged"
+        Visit.first
+    end
 end
--- v2.1-rails/app/consumers/visits_consumer.rb
+++ v2.1-rails/app/consumers/visits_consumer.rb
@@ -3,7 +3,7 @@
 # Example consumer that consumes the events topic and stores data into events table
 class VisitsConsumer < ApplicationConsumer
   # Consumes the messages by inserting all of them in one go into the DB
-  def consume
+  def consume_one
     ::Visit.insert_all messages.payloads
   end
 end
