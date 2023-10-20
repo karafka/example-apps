edd04b8d011aa27d2165ab372607ff0947cfec84
Maciej Mensfeld <maciej@mensfeld.pl>
2023-10-20 16:10:00 +0200

    gems bump

--- v2.2-rails/db/schema.rb
+++ v2.2-rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_12_20_170247) do
+ActiveRecord::Schema[7.1].define(version: 2021_12_20_170247) do
   create_table "visitors", id: :string, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
