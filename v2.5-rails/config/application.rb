ce6c0e0b912c2dfcaca07a0eaa4271ad1c792774
Maciej Mensfeld <maciej@mensfeld.pl>
2025-12-18 13:11:13 +0100

    repro mini setup

--- v2.5-rails/config/application.rb
+++ v2.5-rails/config/application.rb
@@ -13,7 +13,7 @@ module ExampleApp
   # Our example app entry-point
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.1
 
     config.active_job.queue_adapter = :karafka
 
--- v2.5-rails/karafka.rb
+++ v2.5-rails/karafka.rb
@@ -50,7 +50,3 @@ class KarafkaApp < Karafka::App
 end
 
 Karafka::Web.enable!
-
-# You can tag your processes with any info you want and it is going to be visible via the Web UI
-git_hash = `git rev-parse --short HEAD`.strip
-Karafka::Process.tags.add(:commit, "##{git_hash}")
